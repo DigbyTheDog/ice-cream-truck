7411a7417c9456da94f3a25c0d170efd70ebd232
Lex Leesch <leescha@mail.gvsu.edu>
2025-01-11 11:39:57 -0800

    Add popsicle material back

--- src/render_script.py
+++ src/render_script.py
@@ -1,10 +1,10 @@
 import bpy
 
-file_path='final.png'
+file_path='../../venv/final.png'
 popsicle_width=0.3
 move_up=0.2
 
-bpy.ops.wm.open_mainfile(filepath="Test.blend")
+bpy.ops.wm.open_mainfile(filepath="Popsicle.blend")
 
 bpy.ops.image.import_as_mesh_planes(shader='SHADELESS', files=[{'name':file_path}])
 
@@ -12,15 +12,15 @@ plane = bpy.context.object
 if plane is None:
     raise RuntimeError("No plane was imported. Check your image path.")
 
-#material_name = "Popsicle"
-#if material_name in bpy.data.materials:
-#    material = bpy.data.materials[material_name]
-#else:
-#	raise RuntimeError("Popsicle Material not found. Check materials.")#
+material_name = "Popsicle"
+if material_name in bpy.data.materials:
+    material = bpy.data.materials[material_name]
+else:
+	raise RuntimeError("Popsicle Material not found. Check materials.")#
 
-## Assign the material to the popsicle
-#plane.data.materials.clear()
-#plane.data.materials.append(material)
+# Assign the material to the popsicle
+plane.data.materials.clear()
+plane.data.materials.append(material)
 
 # Extrude that bad boy
 bpy.ops.object.mode_set(mode='EDIT')
